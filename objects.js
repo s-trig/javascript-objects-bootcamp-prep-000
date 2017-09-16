0ce8dca5820596e76132a55f19a37344ea5459f5
David Reep <davereep@gmail.com>
2017-09-16 20:28:25 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {artist: "song"}
+
+function updatePlaylist(obj, artist, song) {
+  return Object.assign({}, obj, {}[key]: value})
+}
+
+function removeFromPlaylist (obj, artist) {
+  delete obj.artist
+}
